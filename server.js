651611fb957ed10a0783b76570bb35bcc1afdb48
Y <ptifur@gmail.com>
2020-09-12 09:59:53 +0300

    update route

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ app.get('/', (req, res) => res.render('home'))
 app.get('/books', async (req, res) => {
     try {
         const client = await pool.connect()
-        const result = await client.query('SELECT * FROM booklist');
-        const results = { 'results': (result) ? result.rows : null};
-        res.render('book-list', results );
-        client.release();
-    } catch(err) {
+        const result = await client.query('SELECT * FROM booklist')
+        const results = { 'results': (result) ? result.rows : null}
+        res.render('book-list', results )
+        client.release()
+    } catch (err) {
         console.log(err)
         res.send(err)
     }
@@ -46,7 +46,7 @@ app.post('/books/submit', async (req, res) => {
         client.query(sql, params)
         client.release();
         res.redirect('/books')
-    } catch(err) {
+    } catch (err) {
         res.send(err)
     }
 })
@@ -59,8 +59,43 @@ app.get('/books/delete/:id', async (req, res) => {
         client.query(sql, params)
         client.release()
         res.redirect('/books')
-    } catch(err) {
+    } catch (err) {
         res.send(err)
+    }
+})
+
+app.get('/books/edit/:id', async (req, res) => {
+    try {
+
+        const client = await pool.connect()
+
+        const sql = 'SELECT * FROM booklist WHERE id = $1'
+        const params = [req.params.id]
+
+        const result = await client.query(sql, params)
+
+        res.render('book-update', { r: result.rows[0] })
+
+    } catch (err) {
+        res.send(err)
+    }
+})
+
+app.post('/books/edit/:id', async (req, res) => {
+    try {
+
+        const client = await pool.connect()
+
+        const sql = 'UPDATE booklist SET title = $1, author = $2 WHERE id = $3'
+        const params = [req.body.title, req.body.author, req.params.id]
+
+        const result = await client.query(sql, params)
+
+        res.redirect('/books')
+
+    } catch (err) {
+        res.send(err)
+        res.redirect('/')
     }
 })
 
